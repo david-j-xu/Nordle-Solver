116815a1fdd3d29f77fb9ad3558597d2fe88867a
david-j-xu <63666552+davidjxu21@users.noreply.github.com>
2022-03-31 15:52:36 -0400

    Implemented simple wordle stuff
    Co-authored-by: krishyshah <krishyshah@users.noreply.github.com>

--- bin/utils.py
+++ bin/utils.py
@@ -1,41 +1,104 @@
-from wordle import Wordle
-from typing import Set
-
-from main import logger
-
-'''
-Labels a word w1 given true word w2
-'''
-
-
-def label(w1: str, w2: str):
-    raise NotImplementedError
-
+from typing import Set, List
+from enum import Enum
+from xmlrpc.client import Boolean
 
 '''
-Class encoding a candidate action
+Enumeration of hint types
 '''
 
 
-class Action:
-    guess: str
-    entropy: float
-    word_set: Set[str]
+class Hint(Enum):
+    GREY = 1
+    YELLOW = 2
+    GREEN = 3
 
 
 '''
-Calculates entropy (\E[-\lg(x)], where x denotes the proportion of remaining words
+Class encoding a label
 '''
 
 
-def get_entropy(guess: str, word_set: Set[str]) -> float:
-    raise NotImplementedError
+class Label:
+    guess: List[str]
+    colors: List[Hint]
+
+    def __init__(self, guess: List[str]) -> None:
+        self.guess = guess
+        self.colors = [Hint.GREY for _ in range(5)]
+
+    def update(self, idx: int, color: Hint) -> None:
+        if (idx < 0 or idx > 4):
+            raise ValueError("Must only give hints for valid indices")
+
+        self.colors[idx] = color
+
+    def __str__(self) -> str:
+        return ' '.join([self.guess[i] + ':' + self.colors[i].__str__() for i in range(5)])
 
 
 '''
-Calculates best action
+Labels a candidate word w1 given true word w2
 '''
 
 
-def best_action(game: Wordle) -> Action:
-    raise NotImplementedError
+def label(w1: List[str], w2: List[str]):
+    if (not (len(w1) == len(w2) == 5)):
+        raise ValueError("Words must be of length 5")
+
+    new_label = Label(w1.copy())
+
+    w1p = w1.copy()
+    w2p = w2.copy()
+
+    # label all green elements
+    for i in range(5):
+        if w1p[i] == w2p[i]:
+            new_label.update(i, Hint.GREEN)
+            w1p[i] = '$'
+            w2p[i] = '$'
+
+    # label all yellow elements
+    for i in range(5):
+        if w1p[i] != '$':
+            for j in range(5):
+                if w1p[i] == w2p[j]:
+                    new_label.update(i, Hint.YELLOW)
+                    w2p[j] = '$'
+                    break
+
+    return new_label
+
+
+'''
+Helper function checking consistency of word w with labels l
+'''
+
+
+def check_valid_word(w: List[str], l: Label) -> Boolean:
+    if len(w) != 5:
+        raise ValueError("Word must be of length 5")
+    # word is valid iff it has the same coloring against the guess as the true word
+    guess_word = l.guess
+    cand_lbl = label(guess_word, w)
+    for i in range(5):
+        if cand_lbl.colors[i] != l.colors[i]:
+            return False
+    return True
+
+
+'''
+Helper function to create list of strings from string
+'''
+
+
+def guess_of_str(guess: str) -> List[str]:
+    if len(guess) != 5:
+        raise ValueError("Incorrect length of string")
+    return [guess.lower()[i] for i in range(5)]
+
+
+if __name__ == "__main__":
+    w1 = guess_of_str("wordl")
+    w2 = guess_of_str("wordq")
+    label(w1, w2)
+    print(w1, w2)
